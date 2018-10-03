1e701317b5b9e8a7a9fc822057b126ec8913d108
Saint-Jean Germila <germila@noukod.com>
2018-10-03 18:51:00 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,11 +2,12 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
- BRANDS =[]
+   BRANDS =[]
 
   def initialize(brand)
     @brand = brand
     BRANDS << brand
+    BRANDS << self.brand
   end
 
   def cobble
